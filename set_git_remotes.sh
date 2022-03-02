c9fdc87c359354b806167fd7bd23b6341fbb4300
vanschalexan <alexander.vanschie@fhgr.ch>
2022-03-02 14:33:32 +0100

    bash script for remote repos

---
+++ set_git_remotes.sh
@@ -0,0 +1,4 @@
+git remote add github git@github.com:orbis-eval/orbis_2.git
+git remote set-url --add --push github git@github.com:orbis-eval/orbis_2.git
+git remote add gitlab gitlab@gitlab.semanticlab.net:futureofwork/orbis_2_webservice.git
+git remote set-url --add --push gitlab gitlab@gitlab.semanticlab.net:futureofwork/orbis_2_webservice.git
