87376dbd069db4837cf85ac0fe8ec317d2b03ba4
Philipp Kuntschik <philipp.kuntschik@fhgr.ch>
2022-06-10 13:45:57 +0000

    Update src/db.py

--- src/db.py
+++ src/db.py
@@ -91,6 +91,7 @@ class DB:
 
     async def _add_annotations(self, da_id, d_id, record):
         # check if d_id is equal, replace d_id in record with param.
+        
         if record.get("d_id") != d_id:
             print(f'conflicting d_id detected: {d_id}, overwriting..')
             record["d_id"] = d_id
@@ -208,6 +209,7 @@ class DB:
                                                         annotator=annotator,
                                                         iteration_id=data['meta'].get('iteration', None),
                                                         precessor=da_id)
-        print(f'Now adding new annotation da_id: {da_id}')                                                
+        print(f'Now adding new annotation da_id: {da_id}')                     
+        print(f'debug record {data}')                           
         annotation_id = self._add_annotations(new_da_id, d_id, data.get("data", []))
         return new_da_id
