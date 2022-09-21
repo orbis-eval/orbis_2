6f9d75e6cb60f328de304846e1e7674ad58c63b8
vanschalexan <alexander.vanschie@fhgr.ch>
2022-09-21 09:44:48 +0200

    changed document loader to add documents to annotator queue

--- corpus/document_loader.py
+++ corpus/document_loader.py
@@ -13,15 +13,23 @@ def import_into_db(gold_dir, service_url, max_files=5, prod=False):
     for f in files[:max_files]:
         with open(f, 'r+') as file:
             data = json.loads(file.read())
-            r = session.post(service_url, json=data)
-            if json.loads(r.content.decode())['status_code'] == 200:
+            r = session.post(f'{service_url}addDocument', json=data)
+            r = json.loads(r.content.decode())
+            if r['status_code'] == 200:
                 print(f'Document {f} added.')
+                da_id = r['content']['da_id']
+                r2 = session.put(f'{service_url}addDocumentToAnnotationQueue/{da_id}')
+                if json.loads(r2.content.decode())['status_code'] == 200:
+                    print(f'Document {f} added to annotator queue.')
+                else:
+                    print(f'Document {f} not added to annotator queue.')
             else:
                 print(f'Document {f} not added.')
 
 
 if __name__ == '__main__':
-    # LIVE_SERVICE_URL = 'https://orbis2.prod.semanticlab.net/addDocument'
-    # LOCAL_SERVICE_URL = 'http://0.0.0.0:63010/addDocument'
+    # LIVE_SERVICE_URL = 'https://orbis2.prod.semanticlab.net/'
+    LOCAL_SERVICE_URL = 'http://0.0.0.0:63010/'
     GOLD_DIR = Path(__file__).parent / 'goldDocuments' / 'orbis'
-    import_into_db(GOLD_DIR, os.environ['SERVICE_URL'], 1000, True)
+    # import_into_db(GOLD_DIR, os.environ['SERVICE_URL'], 1000, True)
+    import_into_db(GOLD_DIR, LOCAL_SERVICE_URL, 1000, True)
