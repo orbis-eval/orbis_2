26a5f2b3ffc6ae76895a3dfba793a6800c4c1811
vanschalexan <alexander.vanschie@fhgr.ch>
2022-09-21 07:26:53 +0200

    update remotes to fhgr infrastructure and included frontend documentation

--- set_git_remotes.sh
+++ set_git_remotes.sh
@@ -1,4 +1,4 @@
 git remote add github git@github.com:orbis-eval/orbis_2.git
 git remote set-url --add --push github git@github.com:orbis-eval/orbis_2.git
-git remote add gitlab gitlab@gitlab.semanticlab.net:futureofwork/orbis_2_webservice.git
-git remote set-url --add --push gitlab gitlab@gitlab.semanticlab.net:futureofwork/orbis_2_webservice.git
+git remote add gitlab git@git.fhgr.ch:nlp/project/future-of-work/orbis2.git
+git remote set-url --add --push gitlab git@git.fhgr.ch:nlp/project/future-of-work/orbis2.git
